98e3736db7312a6f1c0be2420335ca4ec905f3e6
24227537 <24227537@student.uwa,edu.au>
2026-05-10 01:30:06 +0800

    Add named entity recognition module with spaCy integration and fallback support.

--- app/bot/ner.py
+++ app/bot/ner.py
@@ -1,7 +1,12 @@
 import spacy
 
-nlp = spacy.load('en_core_web_sm')
+try:
 
+    nlp = spacy.load('en_core_web_sm')
+
+except OSError:
+
+    nlp = spacy.blank('en')
 
 def extract_entities(text):
 
@@ -10,9 +15,13 @@ def extract_entities(text):
     entities = []
 
     for ent in doc.ents:
+
         entities.append({
+
             'text': ent.text,
+
             'label': ent.label_
+
         })
 
     return entities
