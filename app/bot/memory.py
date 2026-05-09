a2437a1af2e7ba3770fe09ac9c289b987d3d3ccf
24227537 <24227537@student.uwa,edu.au>
2026-05-10 00:09:56 +0800

    Updated the bot memory to import the other memory matching files in the bot folder.

--- app/bot/memory.py
+++ app/bot/memory.py
@@ -1,6 +1,9 @@
 from app import db
 from app.models import BotMemory
 
+from app.bot.topic_classifier import classify_topic
+from app.bot.ner import extract_entities
+
 
 IMPORTANT_WORDS = [
     'favorite',
@@ -11,20 +14,61 @@ IMPORTANT_WORDS = [
 ]
 
 
+MEMORY_THRESHOLD = 2
+
+
+
+def calculate_memory_score(message):
+
+    score = 0
+
+    text = message.lower()
+
+    for word in IMPORTANT_WORDS:
+        if word in text:
+            score += 2
+
+    entities = extract_entities(message)
+
+    score += len(entities)
+
+    if len(message.split()) > 10:
+        score += 1
+
+    return score
+
+
+
 def store_memory(user, message):
 
-    words = message.lower().split()
+    existing = BotMemory.query.filter_by(
+        user_id=user.id,
+        memory_value=message
+    ).first()
 
-    for word in words:
+    if existing:
+        return
 
-        if word in IMPORTANT_WORDS:
+    memory_score = calculate_memory_score(message)
 
-            memory = BotMemory(
-                user_id=user.id,
-                memory_key=word,
-                memory_value=message
-            )
+    if memory_score < MEMORY_THRESHOLD:
+        return
 
-            db.session.add(memory)
+    topic = classify_topic(message)
 
+    entities = extract_entities(message)
+
+    entity_summary = ', '.join([
+        f"{e['text']}:{e['label']}"
+        for e in entities
+    ])
+
+    memory = BotMemory(
+        user_id=user.id,
+        memory_key=topic,
+        memory_value=message,
+        importance=memory_score
+    )
+
+    db.session.add(memory)
     db.session.commit()
